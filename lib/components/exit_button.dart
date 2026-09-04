653155fd1a7c776440e5b04b45e1e188aebb49c2
Sultan Ahmad <106477845+Agannnnn@users.noreply.github.com>
2026-09-05 04:50:20 +0700

    add: new app exit button

--- lib/components/exit_button.dart
+++ lib/components/exit_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ExitButton extends StatefulWidget {
   const new({super.key});
@@ -12,7 +13,7 @@ class _ExitButtonState extends State<ExitButton> {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        debugPrint("======================= EXITING =====================");
+        SystemNavigator.pop();
       },
       child: Container(
         decoration: BoxDecoration(
